a1b15149eb3feaf644719df58d3af75a5c01f28b
unknown <alvaro@.alvarogonzalez.educa.madrid.org>
2011-10-21 10:14:02 +0200

    Peleando con git, ya lo he movido y ahora no sale en el github

--- Practice - resueltos/Welcome/Welcome.rb
+++ Practice - resueltos/Welcome/Welcome.rb
@@ -44,15 +44,64 @@ class GCJCase < GCJCase_Base
     self
   end
     
-    
-  def contains_all( s, pattern )
-    s.each_char{ |c|
-      return false if pattern.index(c) == nil
-    }
-    return true
+
+  def log_comb( s, t, comb )
+    # JUST FOR TESTING, NOT USED
+    t.each_char{ |c| $iolog.print "\t" + c }
+    $iolog.puts
+    for sIndex in (0 ... s.length ) do
+      $iolog.print s[sIndex] 
+      comb.each{ |co|
+        $iolog.print "\t" + co[sIndex].to_s
+      }
+      $iolog.print "\n"
+    end
+    $iolog.print "\n"
   end
   
+  def modulus(i)
+    i % 10000
+  end
+    
+  def compute_ocurrences( s, t )
   
+    # ARRAY OF COMBINATIONS
+    comb = []
+    t.each_char{ comb << Array.new( s.length, 0 ) }
+    
+    # EACH LAST CHAR OF s COUNTS AS ONE OCURRENCE OF HIMSELF IN t
+    sIndex = 0
+    comb.each_index{ |tIndex|
+      comb[tIndex][sIndex] = 1 if t[tIndex] == s[sIndex]
+    }
+    
+    # LOOK UP EACH CHAR OF s 
+    for sIndex in (1 ... s.length ) do
+      for tIndex in (0 ... t.length ) do
+        next if t[tIndex] != s[sIndex]
+        
+        # IF THIS CHAR IN THE text IS THE SAME AS THE CHAR IN THE sentence
+        # THEN SUM ALL THE OCURRENCES OF THE sentence UP TO THE sIndex CHARACTER
+        sum = 0
+        for i in (0...tIndex) do
+          sum += comb[i][sIndex-1]
+          sum = modulus(sum)
+        end
+        comb[tIndex][sIndex] = sum
+      end
+    end
+    
+    
+    # THE RESULT IS THE SUM OF ALL THE OCURRENCES ADDED FOR THE LAST CHARARCTER
+    sum = 0
+    comb.each_index{ |tIndex|
+      sum += comb[tIndex][-1]
+      sum = modulus(sum)
+    }
+    sum
+    
+  end
+    
   def solve
     @solution = "0000"
     
@@ -60,63 +109,26 @@ class GCJCase < GCJCase_Base
     begin
       new_text = ""
       text.each_char{ |c| (new_text << c ) if SENTENCE.index(c) }
-      log text + "\n---->\n" + new_text + "\n"
       @text = new_text
-    end
+    end if false
     
-    # COMPLEX OPTIMIZATION: REMOVE EVERY LETTER NOT POSSIBLE
-    begin
-      new_text = ""
-      for i in (0...text.length) do
-      
-        char = text[i]
-        if i == 0 || i == text.length-1
-          new_text << char
-          next
-        end
-        
-        prefix = text[0..i-1]
-        suffix = text[i+1..-1]
-        sprefix = SENTENCE[0..SENTENCE.index(char)-1]
-        sprefix = "" if SENTENCE.index(char)==0
-        ssuffix = SENTENCE[SENTENCE.rindex(char)+1..-1]
-        ssuffix = "" if SENTENCE.rindex(char)==SENTENCE.length-1
-        
-        log( "i:" + i.to_s )
-        log( "char:" + char )
-        log( "prefix:" + prefix )
-        log( "suffix:" + suffix )
-        log( "sprefix:" + sprefix )
-        log( "ssuffix:" + ssuffix )
-        
-        # CHECK THERE ARE ENOUGH LETTERS BEFORE AND AFTER i
-        if contains_all( sprefix, prefix )
-          if contains_all( ssuffix, suffix )
-            new_text << char
-            log "  << metido:" + char
-          end
-        end
-      end
-      log "\n" + text + "\n---->\n" + new_text + "\n"
-      @text = new_text
-      
-    end
+    ocurrences = compute_ocurrences( SENTENCE, text )
+    raise "error" if text.length <=30 && ocurrences != compute_ocurrences_lamer( SENTENCE, text )
     
-    
-    ocurrences = compute_ocurrences_lammer( SENTENCE, text )
-    
-    @solution = ocurrences.to_s
+    @solution = sprintf( "%04d", ocurrences )
     self
   end
   
-  def compute_ocurrences_lammer( s, t )
+  def compute_ocurrences_lamer( s, t )
+    # USED FOR TESTING IN THE SMALL CASE
   
     return t.count(s) if s.length == 1 
   
     first = s[0]
     ocurrences = 0
     for c in (0 ... t.length) do
-      ocurrences += compute_ocurrences_lammer( s[1..-1], t[c+1..-1] ) if t[c] == first 
+      ocurrences += compute_ocurrences_lamer( s[1..-1], t[c+1..-1] ) if t[c] == first 
+      ocurrences = modulus(ocurrences)
     end
     ocurrences
   end
@@ -127,3 +139,4 @@ end
 
 GCJProblems.new.solve
   
+
