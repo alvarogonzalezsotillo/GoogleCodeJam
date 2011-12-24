5168a0ff88ef7806d97fb4a24137269c58fc86f2
unknown <alvaro@.alvarogonzalez.educa.madrid.org>
2011-12-24 09:44:22 +0100

    NumberGame ya funciona, pero en 35 minutos en mi acer travelmate 290

--- Practice/ImOnIt/NumberGame/NumberGame.rb
+++ Practice/ImOnIt/NumberGame/NumberGame.rb
@@ -31,6 +31,8 @@ end
 #
 class GCJCase < GCJCase_Base
 
+  INFINITE = 1000001
+  @@constant = nil
 
   attr_reader :a1, :a2, :b1, :b2
   
@@ -40,9 +42,14 @@ class GCJCase < GCJCase_Base
   end
   
   def solve
-    #compute_first_win( a1, a2 )
-    #@solution = "hola"
-    real_solve()
+    sh = solve_hacker() 
+
+    if (a2-a1)<30 && (b2-b1)<30
+      sl = solve_lamer()
+      raise "WTF #{a1} #{a2} #{b1} #{b2} -- #{sl} #{sh}" if sl != sh
+    end
+    
+    @solution = sh.to_s
     self
   end
   
@@ -55,14 +62,10 @@ class GCJCase < GCJCase_Base
 
       # ENSURE a IS BIGGER THAN b
       (a,b) = [b,a] if a < b
-      ##log( "a: #{a}  b: #{b}  firstPlayer:#{firstPlayer}")
     
       # COMPUTE POSSIBLE NEXT MOVES
       greatestBite = ( a/b ).floor
       canReachZero = (a % b) == 0
-
-      #log( "  greatestBite: #{greatestBite}" )
-      #log( "  canReachZero: #{canReachZero}" )
 
       if greatestBite > 1 && canReachZero
         aSmall = a - b*(greatestBite-1)
@@ -77,10 +80,8 @@ class GCJCase < GCJCase_Base
         aSmall = a - b*greatestBite
         aBig = aSmall
       end
-      #log( "  aBig: #{aBig}  aSmall: #{aSmall}" )
 
       thereIsChoice = aBig != aSmall
-      #log( "  thereIsChoice: #{thereIsChoice}" )
       
       # IF THERE IS CHOICE FOR FIRST PLAYER, HE WINS
       return firstPlayer if thereIsChoice
@@ -100,20 +101,155 @@ class GCJCase < GCJCase_Base
     
   end
 
+  def solve_hacker()
     
-  def real_solve()
-    @solution = 0
+=begin    
+    a   
+   ########### ldb
+  #  #########
+b1#   ########
+  ##    ######
+  ###     #### fad
+  ###      ###
+  ####       #
+b2####
+  #####
+  ######
+  ######
+  #######
+=end
+    
+    (@a1,@a2,@b1,@b2) = [b1,b2,a1,a2] if (a2-a1) > (b2-b1)
+
+    overlap = 0
+    counter = 0    
+    for a in (a1..a2) do
+      lbd = estimate_last_before_diagonal(a)
+      fad = estimate_first_after_diagonal(a)
+      
+      # COMPUTE OVERLAP BETWEEN 
+      #  [1..lbd] U [fad..infinite] 
+      # AND
+      #  [b1..b2]
+      overlap += segment_overlap(1,lbd,b1,b2)
+      overlap += segment_overlap(fad,INFINITE,b1,b2)
+      counter += 1
+      log( (1.0*(a2-a1-counter)/(a2-a1)).to_s ) if counter%10000==0
+    end
+    
+    overlap
+  end
+  
+  def segment_overlap(x1,x2,y1,y2)
+    ret = 0
+    if x1.between?(y1,y2) || x2.between?(y1,y2) || y1.between?(x1,x2) || y2.between?(x1,x2)
+      min = [x1,y1].max
+      max = [x2,y2].min
+      ret += max-min+1
+    end
+    #log( "    overlap #{x1},#{x2}  #{y1},#{y2} -- #{ret}" )
+    ret
+  end
+    
+  def solve_lamer()
+    ret = 0
     for a in (a1 .. a2) do
       for b in (b1 .. b2) do
-        @solution += 1 if compute_first_win(a,b)
+        ret += 1 if compute_first_win(a,b)
       end
     end
+    ret
   end
+  
+  def draw_shape(a1,a2,b1,b2)
+    for a in (a1..a2) do
+      print a.to_s + "\t"
+      for b in (b1..b2) do
+        w = compute_first_win(a,b)
+        print (w ? "#" : " ")
+      end
+      puts
+    end
+  end
+
+  def compute_first_after_diagonal(a)
+    first = 0
+    b = a+1
+    while first == 0
+      first = b if compute_first_win(a,b)
+      b += 1
+    end
+    return first
+  end
+
+  def compute_last_before_diagonal(a)
+    return 0 if a == 1
+    
+    last = 0
+    b = a-1
+    while last == 0
+      raise "WFT" if b <= 0
+      last = b if compute_first_win(a,b)
+      b -= 1
+    end
+    return last
+  end
+  
+  def estimate_first_after_diagonal(a)
+    hint = (a*constant()).ceil
+    # CHANCHULLO FACTOR: SOMETIMES THE PRECISSION IS NOT ENOUGH
+    return hint-1 if compute_first_win(a,hint-1)
+    hint
+  end
+
+  def estimate_last_before_diagonal(a)
+    hint = (a/constant()).ceil - 1
+    # CHANCHULLO FACTOR: SOMETIMES THE PRECISSION IS NOT ENOUGH
+    return hint+1 if compute_first_win(a,hint+1)
+    hint
+  end
+
+  
+  def constant()
+    return @@constant if @@constant != nil
+    big_big_number = INFINITE
+    @@constant = Rational(compute_first_after_diagonal(big_big_number),big_big_number)
+  end
+
+  def display_constant_up_to(size)
+    for a in (1..size) do
+      first = compute_first_after_diagonal(a)
+      puts "#{a}: #{first}  -- #{(1.0*a/first)}"
+    end
+  end
+  
+  
+  def check_constant(a1,a2)
+    constant = constant()
+    for i in (a1..a2) do
+      first = compute_first_after_diagonal(i)
+      prevision_first = estimate_first_after_diagonal(i)
+      hit_first = first == prevision_first
+      
+      last = compute_last_before_diagonal(i)
+      prevision_last = estimate_last_before_diagonal(i)
+      hit_last = last == prevision_last
+
+      puts "first " + (hit_first ? "HIT " : "FAIL") + " #{i}: #{first} -- #{prevision_first}"
+      puts "last  " + (hit_last ? "HIT " : "FAIL") + " #{i}: #{last} -- #{prevision_last}"
+      
+      raise "FAIL" if !hit_first || !hit_last
+    end
+  end
+  
 
 end
 
 ####################################################################################
 
-GCJProblems.new.solve
   
+#GCJCase.new( nil, nil ).draw_shape(43784-10, 43784+10, 27060-10, 27060+10)
+#GCJCase.new( nil, nil ).compute_constant_up_to(60)
+#GCJCase.new( nil, nil ).check_constant(43784-10, 43784+10)
 
+GCJProblems.new.solve
