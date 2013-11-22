78194ca9fc3fe130b1c55de64600715ca6be964b
alvaro <alvarogonzalezsotillo@gmail.com>
2013-11-23 00:55:03 +0100

    refactored to use ordering

--- Hackercup2013-11/BasketballGame/src/contest/BasketballGame.scala
+++ Hackercup2013-11/BasketballGame/src/contest/BasketballGame.scala
@@ -1,90 +1,89 @@
 package contest
 
 /**
-  * Created with IntelliJ IDEA.
-  * User: alvaro
-  * Date: 22/11/13
-  * Time: 11:16
-  * To change this template use File | Settings | File Templates.
-  */
-object BasketballGame extends App with ContestStub{
+ * Created with IntelliJ IDEA.
+ * User: alvaro
+ * Date: 22/11/13
+ * Time: 11:16
+ * To change this template use File | Settings | File Templates.
+ */
+object BasketballGame extends App with ContestStub {
 
 
-  solveAll("BasketballGame")
 
   def extractOneTest(lineIterator: LineIterator): LoadedTest = {
     val header = lineIterator.next
-    val Array(n,m,p) = header.split("""\s+""").map( _.toInt )
+    val Array(n, m, p) = header.split( """\s+""").map(_.toInt)
     val players = lineIterator.take(n).toList
     (header :: players).toArray
   }
 
-  def solveOneTest(loadedTest: LoadedTest): Solution = {
-  
-    val Array(n,m,p) = loadedTest.head.split("""\s+""").map( _.toInt )
-  
-    class Player(val name:String, val shotP: Int, val height: Int){
-      var draftNumber = 0
-      var timePlayed = 0
-      override def toString = name
+  class Player(val name: String, val shotP: Int, val height: Int) {
+    var draftNumber = 0
+    var timePlayed = 0
+    override def toString = name
+  }
+
+  // FIRST SHOULD LEFT COURT
+  lazy val onCourtOrdering = new Ordering[Player] {
+    def compare(x: Player, y: Player) = {
+      if (x.timePlayed != y.timePlayed) y.timePlayed - x.timePlayed
+      else y.draftNumber - x.draftNumber
     }
-    
-    
-    val players = loadedTest.tail.map{ s =>
-      val Array(name,shotP,height) = s.split("""\s+""")
+  }
+
+  // FIRST WILL ENTER COURT
+  lazy val onBenchOrdering = new Ordering[Player] {
+    def compare(x: Player, y: Player): Int = {
+      if (x.timePlayed != y.timePlayed) x.timePlayed - y.timePlayed
+      else x.draftNumber - y.draftNumber
+    }
+  }
+
+
+  def solveOneTest(loadedTest: LoadedTest): Solution = {
+
+    val Array(_, m, p) = loadedTest.head.split( """\s+""").map(_.toInt)
+
+    val players = loadedTest.tail.map {  s =>
+      val Array(name, shotP, height) = s.split( """\s+""")
       new Player(name, shotP.toInt, height.toInt)
     }
-    
-    val playersSortedByDraft = players.sortWith{ (p1,p2) =>
-      if( p1.shotP != p2.shotP ) p1.shotP > p2.shotP
+
+    val playersSortedByDraft = players.sortWith {  (p1, p2) =>
+      if (p1.shotP != p2.shotP) p1.shotP > p2.shotP
       else p1.height > p2.height
     }
-    playersSortedByDraft.zipWithIndex.foreach{ case (player,index) => player.draftNumber = index+1 }
-    
-    
-    def play( playersSortedByDraft: Iterable[Player] ) : List[Player] = {
+    playersSortedByDraft.zipWithIndex.foreach {
+      case (player, index) => player.draftNumber = index + 1
+    }
 
-      if( playersSortedByDraft.size == p ){
-        return playersSortedByDraft.toList
-      }
+    val team1 = playersSortedByDraft.zipWithIndex.filter(_._2 % 2 == 0).map(_._1)
+    val team2 = playersSortedByDraft.zipWithIndex.filter(_._2 % 2 == 1).map(_._1)
 
+    def computePlayersOnCourt(playersSortedByDraft: Iterable[Player]) = {
       val playersOnCourt = new collection.mutable.HashSet ++ playersSortedByDraft.take(p).toSet
       val playersOnBench = new collection.mutable.HashSet ++ playersSortedByDraft.drop(p).toSet
-      
-      for( rotation <- 1 to m if p < players.size ){
-        playersOnCourt.foreach( _.timePlayed += 1 )
-        
-        val playerOut = playersOnCourt.foldLeft(playersOnCourt.head){ (p1,p2) =>
-          if( p1.timePlayed > p2.timePlayed ) p1
-          else if( p1.timePlayed < p2.timePlayed ) p2
-          else if( p1.draftNumber < p2.draftNumber ) p2
-          else p1
-        }
-        
-        val playerIn = playersOnBench.foldLeft(playersOnBench.head){ (p1,p2) =>
-          if( p1.timePlayed < p2.timePlayed ) p1
-          else if( p1.timePlayed > p2.timePlayed ) p2
-          else if( p1.draftNumber < p2.draftNumber ) p1
-          else p2
-        }
-        
-        playersOnCourt -= playerOut
-        playersOnCourt += playerIn
-        playersOnBench += playerOut
-        playersOnBench -= playerIn
-        
-        log( s"$rotation: out:$playerOut in:$playerIn onCourt:${playersOnCourt.mkString(" ")}  onBench:${playersOnBench.mkString(" ")}" )
-      }
-      
-      playersOnCourt.toList
-    }
-    
-    val team1 = playersSortedByDraft.zipWithIndex.filter( _._2 % 2 == 0 ).map( _._1 )
-    val team2 = playersSortedByDraft.zipWithIndex.filter( _._2 % 2 == 1 ).map( _._1 )
 
-    val playersOnCourt = play(team1) ::: play(team2)
-    
-    playersOnCourt.map( _.name ).sorted.mkString( " " )
+      for (rotation <- 1 to m if playersOnBench.size > 0) {
+        playersOnCourt.foreach(_.timePlayed += 1)
+        val playerOut = playersOnCourt.min(onCourtOrdering)
+        val playerIn = playersOnBench.min(onBenchOrdering)
+
+        playersOnCourt -= playerOut += playerIn
+        playersOnBench += playerOut -= playerIn
+
+        log(s"$rotation: out:$playerOut in:$playerIn onCourt:${playersOnCourt.mkString(" ")}  onBench:${playersOnBench.mkString(" ")}")
+      }
+
+      playersOnCourt
+    }
+
+    val playersOnCourt = computePlayersOnCourt(team1) ++ computePlayersOnCourt(team2)
+    playersOnCourt.toSeq.map(_.name).sorted.mkString(" ")
   }
+
+
+  solveAll("BasketballGame")
 }
 
