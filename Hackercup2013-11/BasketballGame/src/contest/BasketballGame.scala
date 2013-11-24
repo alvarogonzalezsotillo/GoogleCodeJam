3497ce25bf388a456ff134026960bc22698efa78
alvaro <alvarogonzalezsotillo@gmail.com>
2013-11-24 22:03:39 +0100

    Some improvements

--- Hackercup2013-11/BasketballGame/src/contest/BasketballGame.scala
+++ Hackercup2013-11/BasketballGame/src/contest/BasketballGame.scala
@@ -40,8 +40,57 @@ object BasketballGame extends App with ContestStub {
     }
   }
 
-
   def solveOneTest(loadedTest: LoadedTest): Solution = {
+    val ret1 = solveOneTest_lamer(loadedTest)
+    val ret2 = solveOneTest_hacker(loadedTest)
+    assert(ret1 == ret2)
+    ret1
+  }
+
+  def solveOneTest_hacker(loadedTest: LoadedTest): Solution = {
+
+    val Array(_, m, p) = loadedTest.head.split( """\s+""").map(_.toInt)
+
+    val players = loadedTest.tail.map {  s =>
+      val Array(name, shotP, height) = s.split( """\s+""")
+      new Player(name, shotP.toInt, height.toInt)
+    }
+
+    val playersSortedByDraft = players.sortWith {  (p1, p2) =>
+      if (p1.shotP != p2.shotP) p1.shotP > p2.shotP
+      else p1.height > p2.height
+    }
+    playersSortedByDraft.zipWithIndex.foreach {
+      case (player, index) => player.draftNumber = index + 1
+    }
+
+    val team1 = playersSortedByDraft.zipWithIndex.filter(_._2 % 2 == 0).map(_._1)
+    val team2 = playersSortedByDraft.zipWithIndex.filter(_._2 % 2 == 1).map(_._1)
+
+    def computePlayersOnCourt(playersSortedByDraft: Iterable[Player]) = {
+      val playersOnCourt = new collection.mutable.TreeSet()(onCourtOrdering) ++ playersSortedByDraft.take(p)
+      val playersOnBench = new collection.mutable.TreeSet()(onCourtOrdering.reverse) ++ playersSortedByDraft.drop(p)
+
+      for (rotation <- 1 to m if playersOnBench.size > 0) {
+        playersOnCourt.foreach(_.timePlayed += 1)
+        val playerOut = playersOnCourt.head
+        val playerIn = playersOnBench.head
+
+        playersOnCourt -= playerOut += playerIn
+        playersOnBench += playerOut -= playerIn
+
+        log(s"$rotation: out:$playerOut in:$playerIn onCourt:${playersOnCourt.mkString(" ")}  onBench:${playersOnBench.mkString(" ")}")
+      }
+
+      playersOnCourt
+    }
+
+    val playersOnCourt = computePlayersOnCourt(team1) ++ computePlayersOnCourt(team2)
+    playersOnCourt.toSeq.map(_.name).sorted.mkString(" ")
+  }
+
+
+  def solveOneTest_lamer(loadedTest: LoadedTest): Solution = {
 
     val Array(_, m, p) = loadedTest.head.split( """\s+""").map(_.toInt)
 
