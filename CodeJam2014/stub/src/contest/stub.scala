ae3d4631cb9c230517b1c77479e12f1f331b3348
unknown <alvaro@PROFESOR-A24.aula24.local>
2014-04-24 20:56:51 +0200

    New type of stub

---
+++ CodeJam2014/stub/src/contest/stub.scala
@@ -0,0 +1,17 @@
+package contest
+
+import java.util.Scanner
+
+object stub extends App with ContestStub{
+
+  def extractOneTest(lineIterator: LineIterator) = {
+    ???
+  }
+
+
+  def solveOneTest(in: Scanner) = {
+    ???
+  }
+
+  solveAll(".",false)
+}
